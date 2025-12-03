77c2993a239ae733c4234f0a6fb0de8fa13fc714
josehernandezvergara <127722062+josehernandezvergara@users.noreply.github.com>
2025-12-02 23:22:28 -0600

    feat: Implementa logica real de racha (streak)

--- app/crud/progress.py
+++ app/crud/progress.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
-from sqlalchemy import func
+from sqlalchemy import func, cast, Date
 from .. import models, schemas
-from datetime import datetime
+from datetime import datetime, timedelta
 
 def upsert_user_progress(db: Session, user_id: str, progress_in: schemas.UserModuleProgressCreate):
     """
@@ -30,6 +30,51 @@ def get_user_progress(db: Session, user_id: str):
     """Obtiene todo el progreso (por modulo) del usuario actual."""
     return db.query(models.UserModuleProgress).filter(models.UserModuleProgress.user_id == user_id).all()
 
+def calculate_streak(db: Session, user_id: str) -> int:
+    'calcula la racha de actividad del user'
+
+
+    # 1. Obtener fechas de actividad de Quizzes
+    quiz_dates = db.query(cast(models.QuizAttempt.created_at, Date))\
+        .filter(models.QuizAttempt.user_id == user_id)\
+        .all()
+        
+    # 2. Obtener fechas de actividad de Memorama
+    memory_dates = db.query(cast(models.MemoryRun.created_at, Date))\
+        .filter(models.MemoryRun.user_id == user_id)\
+        .all()
+        
+    # 3. Unir y limpiar fechas (Set para eliminar duplicados del mismo día)
+    # La estructura [0] es porque SQLAlchemy devuelve tuplas (fecha,)
+    all_dates = set([q[0] for q in quiz_dates] + [m[0] for m in memory_dates])
+    
+    if not all_dates:
+        return 0
+        
+    # 4. Ordenar de la mas reciente a la mmas antigua
+    sorted_dates = sorted(list(all_dates), reverse=True)
+    
+    today = datetime.now().date()
+    yesterday = today - timedelta(days=1)
+    
+    # 5. ver si la racha esta viva 
+    latest_date = sorted_dates[0]
+    if latest_date != today and latest_date != yesterday:
+        return 0 # Racha rota :(
+        
+    # 6. Contar dias consecutivos anteriores
+    streak = 1
+    current_check = latest_date
+    
+    for i in range(1, len(sorted_dates)):
+        expected_prev_date = current_check - timedelta(days=1)
+        if sorted_dates[i] == expected_prev_date:
+            streak += 1
+            current_check = sorted_dates[i]
+        else:
+            break # Se rompio la r1a ha
+            
+    return streak
 
 def get_user_stats_summary(db: Session, user_id: str) -> schemas.StatsSummary:
     """
